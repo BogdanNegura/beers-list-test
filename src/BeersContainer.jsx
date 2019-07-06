3b56f3d2ef60f5b96d69e5f76b38952d08d9733b
Bogdan Negura <id.bogdann@yahoo.com>
2019-07-06 16:35:29 +0100

    display data from api

--- src/BeersContainer.jsx
+++ src/BeersContainer.jsx
@@ -1,3 +1,33 @@
-import React from 'react';
-import style from "styled-components";
+import React, { useEffect, useState } from 'react';
+// import styled from "styled-components";
 import axios from 'axios';
+
+const BeersContainer = () => {
+  const [ apiBeers, modificaApiBeers ] = useState([])
+  const [ selection, xxxSelection ] = useState(["Beer name"])
+  const [ base, setBase ] = useState("")
+  
+  useEffect(
+    () => {
+      axios 
+        .get(`https://api.punkapi.com/v2/beers`)
+        .then( res => {
+          modificaApiBeers(res.data)
+        })
+    }, []
+  )
+
+
+  return (
+    <div>
+      {apiBeers.map(
+        ({ name, id, ...restulCelorCeErau }) => {
+          console.log(name, restulCelorCeErau)
+          return <p>{name}</p>
+        }
+      )}
+    </div>
+  )
+}
+
+export default BeersContainer
